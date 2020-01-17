5d2d7c34f730dfc0db9bd249639b89e5a1ca672f
almazim89 <alimzagrick@gmail.com>
2020-01-17 17:41:50 +0300

    first commit

---
+++ #source/js/main.js
@@ -0,0 +1,3 @@
+$('.top-bar__burger').click(function () {
+	$('.top-bar__burger-menu').fadeToggle('slow');
+})
---
+++ js/main.js
@@ -0,0 +1,1 @@
+$(".top-bar__burger").click(function(){$(".top-bar__burger-menu").fadeToggle("slow")});
